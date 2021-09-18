7bbc79123f8f31ea1a9a630164b951807d6b6760
Casey Hawkins <hawkcase@oregonstate.edu>
2021-09-18 13:48:06 -0500

    Added lives system and game reset

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Future changes to make:
-# (1) Have enemies spawn and move uniformly
+# (1) -- Completed -- Have enemies spawn and move uniformly -- Completed --
 # (2) --Completed -- When all enemies are defeated, move on to Level 2 -- Completed --
-# (3) Make Level 2 slightly harder than Level 1
+# (3) -- Completed --Make Level 2 slightly harder than Level 1 -- Completed --
 # (4) After Level 2, move on to Level 3
 # (5) Make Level 3 a boss battle of some kind
 # (6) -- Completed -- Implement a pause feature -- Completed --
 # (7) Improve points: Maybe double points for 3 hits without a miss?
 # (8) Allow enemies to randomly drop bombs
-# (9) A way to track lives (3 lives is typical, then game over)
+# (9) -- Completed -- A way to track lives (3 lives is typical, then game over) -- Completed --
 
 from os import spawnl
 import pygame, sys
@@ -44,6 +44,11 @@ class Player(pygame.sprite.Sprite):
         score = score_font.render("Score: " + str(self.score), True, (255, 255, 255))
         screen.blit(score, (x, y))
 
+    def show_lives(self, x, y):
+        # Last tuple value (255, 255, 255) is color of the text being rendered
+        lives_left = lives_font.render("Lives Left: " + str(self.lives), True, (255, 255, 255))
+        screen.blit(lives_left, (x, y))
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, picture_path, pos_x, pos_y, x_change, y_change):
@@ -67,23 +72,7 @@ class Enemy(pygame.sprite.Sprite):
         elif self.pos_x >= 768:
             self.x_change = -self.x_change
             self.pos_y += self.y_change
-
-    def spawnEnemies_1(self):
-        x = 32
-        for newEnemy in range(5):
-            newEnemy = Enemy('enemy.png', 32 + x, 32, 4, 40)
-            enemyGroup.add(newEnemy)
-            x += 96
-        x = 32
-        for newEnemy in range(5):
-            newEnemy = Enemy('enemy.png', 32 + x, 96, 4, 40)
-            enemyGroup.add(newEnemy)
-            x += 96
-        x = 32
-        for newEnemy in range(5):
-            newEnemy = Enemy('enemy.png', 32 + x, 160, 4, 40)
-            enemyGroup.add(newEnemy)
-            x += 96
+ 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, picture_path, pos_x, pos_y, x_change, y_change):
@@ -113,8 +102,8 @@ class Bullet(pygame.sprite.Sprite):
             player.score += 1
             # Collision
             # Reset bullet_state to ready, increase score, and respawn enemy
-            explosion_sound = mixer.Sound('invaderkilled.wav')
-            explosion_sound.play()
+            #explosion_sound = mixer.Sound('invaderkilled.wav')
+            #explosion_sound.play()
             self.pos_y = player.pos_y
             self.bullet_state = "ready"
 
@@ -129,20 +118,6 @@ class GameState():
     def __init__(self, level_tracker=1):
         self.state = 'intro'
         self.level_tracker = 1
-
-    def intro(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                self.state = 'level_1'
-
-        # Drawing
-        screen.blit(background, (0,0))
-        screen.blit(ready_text, (screen_width/2 - 118, screen_height/2 - 40))
-
-        pygame.display.flip()
 
     def level_1(self):
 
@@ -167,17 +142,12 @@ class GameState():
                     player.x_change = 0
             # Press R to restart the game
             if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                enemyGroup.empty()
-                bulletGroup.empty()
-                playerGroup.empty()
-                addPlayer()
-                addEnemies_1()
-                playerGroup.draw(screen)
-                bulletGroup.draw(screen)
-                enemyGroup.draw(screen)
-                player.score = 0
-                self.level_tracker = 1
-                self.state = 'intro'
+                self.reset()
+            # Mute the game
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+                mixer.music.pause()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                mixer.music.unpause()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 bullet = Bullet('bullet.png', player.pos_x, player.pos_y, 0, 10)
                 # Only allow one bullet being fired at a time.
@@ -199,30 +169,18 @@ class GameState():
 
             # Game Over
             if newEnemy.pos_y > 440:
-                print(player.lives)
-                if player.lives == 1:
+                if player.lives == 0:
                     self.state = 'game_over'
                 else:
                     player.lives -= 1
-                    enemyGroup.empty()
-                    bulletGroup.empty()
-                    playerGroup.empty()
-                    addPlayer()
-                    addEnemies_1()
-                    playerGroup.draw(screen)
-                    bulletGroup.draw(screen)
-                    enemyGroup.draw(screen)
-                    player.score = 0
-                    self.level_tracker = 1
-                    self.state = 'intro'
-        # Need to reset the level. Right now it just stays in the same state, so of course my lives run out
-        # quickly and game over.
+                    self.state = 'you_died'
     
         if player.score >= 15:
             self.state = 'level_2_intro'           
 
         # Drawing
         player.show_score(10,10)
+        player.show_lives(590,10)
         playerGroup.draw(screen)
         bulletGroup.draw(screen)
         enemyGroup.draw(screen)
@@ -257,17 +215,7 @@ class GameState():
                     player.x_change = 0
             # Press R to restart the game
             if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                enemyGroup.empty()
-                bulletGroup.empty()
-                playerGroup.empty()
-                addPlayer()
-                addEnemies_1()
-                playerGroup.draw(screen)
-                bulletGroup.draw(screen)
-                enemyGroup.draw(screen)
-                player.score = 0
-                self.level_tracker = 1
-                self.state = 'intro'
+                self.reset()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 bullet = Bullet('bullet.png', player.pos_x, player.pos_y, 0, 12)
                 # Only allow one bullet being fired at a time.
@@ -288,33 +236,54 @@ class GameState():
             newEnemy.enemyMovement()
             # Game Over
             if newEnemy.pos_y > 440:
-                if player.lives == 1:
+                if player.lives == 0:
                     self.state = 'game_over'
                 else:
                     player.lives -= 1
-                    enemyGroup.empty()
-                    bulletGroup.empty()
-                    playerGroup.empty()
-                    addPlayer()
-                    addEnemies_1()
-                    playerGroup.draw(screen)
-                    bulletGroup.draw(screen)
-                    enemyGroup.draw(screen)
-                    player.score = 0
-                    self.level_tracker = 1
-                    self.state = 'intro'
+                    self.state = 'you_died'
         
         if player.score >= 30:
             self.state = 'you_won'           
 
         # Drawing
         player.show_score(10,10)
+        player.show_lives(590,10)
         playerGroup.draw(screen)
         bulletGroup.draw(screen)
         enemyGroup_2.draw(screen)
         
         # Update display module to show screen changes
         pygame.display.update()
+
+    def reset(self):
+        enemyGroup.empty()
+        enemyGroup_2.empty()
+        bulletGroup.empty()
+        player.pos_x = 370
+        player.pos_y = 568
+        player.x_change = 0
+        addEnemies_1()
+        addEnemies_2()
+        playerGroup.draw(screen)
+        bulletGroup.draw(screen)
+        enemyGroup.draw(screen)
+        player.score = 0
+        self.level_tracker = 1
+        self.state = 'intro'
+
+    def intro(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+                self.state = 'level_1'
+
+        # Drawing
+        screen.blit(background, (0,0))
+        screen.blit(ready_text, (screen_width/2 - 118, screen_height/2 - 40))
+
+        pygame.display.flip()
 
     def game_over(self):
         for event in pygame.event.get():
@@ -325,6 +294,19 @@ class GameState():
         # Drawing
         screen.blit(background, (0,0))
         screen.blit(over_text, (screen_width/2, screen_height/2))
+        pygame.display.flip()
+
+    def you_died(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+                self.reset()
+
+        # Drawing
+        screen.blit(background, (0,0))
+        screen.blit(you_died_text, (screen_width/2, screen_height/2))
         pygame.display.flip()
 
     def paused(self):
@@ -372,6 +354,8 @@ class GameState():
             self.level_1()
         if self.state == 'game_over':
             self.game_over()
+        if self.state == 'you_died':
+            self.you_died()
         if self.state == 'paused':
             self.paused()
         if self.state == 'you_won':
@@ -403,12 +387,15 @@ mixer.music.play(-1)
 
 # Font and text for intro
 center_font = pygame.font.Font('freesansbold.ttf', 64)
-ready_text = center_font.render("READY", True, (255, 255, 255))
+ready_text = center_font.render("Level 1", True, (255, 255, 255))
 over_text = center_font.render("GAME OVER", True, (255, 255, 255))
 pause_text = center_font.render("PAUSED", True, (255, 255, 255))
 won_text = center_font.render("YOU WON", True, (255, 255, 255))
 level_2_text = center_font.render("Level 2", True, (255, 255, 255))
+you_died_text = center_font.render("YOU DIED", True, (255, 255, 255))
+
 score_font = pygame.font.Font('freesansbold.ttf', 32)
+lives_font = pygame.font.Font('freesansbold.ttf', 32)
 
 # Player
 # Player image source from https://www.flaticon.com/
@@ -448,24 +435,25 @@ def addEnemies_1():
 addEnemies_1()
 
 # Enemy for Level 2
-global enemyGroup_2
+#global enemyGroup_2
 enemyGroup_2 = pygame.sprite.Group()
-x = 32
-for newEnemy in range(5):
-    newEnemy = Enemy('enemy.png', 32 + x, 32, 6, 80)
-    enemyGroup_2.add(newEnemy)
-    x += 64
-x = 32
-for newEnemy in range(5):
-    newEnemy = Enemy('enemy.png', 32 + x, 96, 6, 80)
-    enemyGroup_2.add(newEnemy)
-    x += 64
-x = 32
-for newEnemy in range(5):
-    newEnemy = Enemy('enemy.png', 32 + x, 160, 6, 80)
-    enemyGroup_2.add(newEnemy)
-    x += 64
-
+def addEnemies_2():
+    x = 32
+    for newEnemy in range(5):
+        newEnemy = Enemy('enemy.png', 32 + x, 32, 6, 80)
+        enemyGroup_2.add(newEnemy)
+        x += 64
+    x = 32
+    for newEnemy in range(5):
+        newEnemy = Enemy('enemy.png', 32 + x, 96, 6, 80)
+        enemyGroup_2.add(newEnemy)
+        x += 64
+    x = 32
+    for newEnemy in range(5):
+        newEnemy = Enemy('enemy.png', 32 + x, 160, 6, 80)
+        enemyGroup_2.add(newEnemy)
+        x += 64
+addEnemies_2()
 # Main game loop
 def main():
     while True:
